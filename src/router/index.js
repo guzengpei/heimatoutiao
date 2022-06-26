432c7d7c21eac4b349dd61f80574ce5f681e80b8
guzengpei <1093569539@qq.com>
2022-06-26 11:44:35 +0800

    路由配置完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Layout from '@/views/Layout'
+const Home = () => import('@/views/Home')
+const Question = () => import('@/views/Question')
+const Video = () => import('@/views/Video')
+const My = () => import('@/views/My')
+const Login = () => import('@/views/Login')
 
 Vue.use(VueRouter)
 
 const routes = [
-
+  {
+    path: '/',
+    component: Layout,
+    redirect: '/home',
+    children: [
+      { path: 'home', component: Home },
+      { path: 'question', component: Question },
+      { path: 'video', component: Video },
+      { path: 'my', component: My }
+    ]
+  },
+  { path: '/login', component: Login }
 ]
 
 const router = new VueRouter({
